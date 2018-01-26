3d2e2d12395ba9fca1aa6f2eb067032e0f053b0a
Hemanth <hemanth.o@techsophy.com>
2018-01-26 12:14:51 +0530

    added log

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.11.11"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % Test
+
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.13"
--- src/test/scala/HelloWorldTest.scala
+++ src/test/scala/HelloWorldTest.scala
@@ -1,8 +1,12 @@
 import org.scalatest.FunSuite
+import org.slf4j.LoggerFactory
+import org.slf4j.Logger
 
 class HelloWorldTest extends FunSuite{
+  val logger: Logger = LoggerFactory.getLogger(this.getClass())
   val helloWorld = new HelloWorld
   test("able get message"){
+    logger.info("testing...")
     assert(helloWorld.greeting("hi") === "hi")
   }
 }

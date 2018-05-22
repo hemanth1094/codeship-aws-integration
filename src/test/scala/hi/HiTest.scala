ffa6dca32e3b115bdd910c4cc5d55d3ee8008324
hemanth <hemanth.o@techsophy.com>
2018-05-22 15:28:19 +0530

    updated test cases

--- src/test/scala/hi/HiTest.scala
+++ src/test/scala/hi/HiTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class HiTest extends FunSuite{
   val hi = new HelloWorld
-  test("get hi message"){
+  test("get hi message123"){
     assert(hi.greeting("hi") === "hi")
   }
 }
